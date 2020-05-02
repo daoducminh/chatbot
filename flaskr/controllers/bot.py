af49505d061d65dd2f32ce0c16ac234382538242
daoducminh <daoducminh1997@gmail.com>
2020-05-02 09:21:15 +0700

    Update bot controller.

--- flaskr/controllers/bot.py
+++ flaskr/controllers/bot.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 bot = Blueprint('bot', __name__)
 
 
-@bot.route('/')
-def test():
-    return 'Hello'
+@bot.route('/init')
+def init_chat_session():
+    pass
+
+
+@bot.route('/chat')
+def chat(text):
+    pass
